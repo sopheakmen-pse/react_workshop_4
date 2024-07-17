c3f5cc6c24000df3736fa75fe7d4359c7bcbd2ea
sopheakmen-pse <sopheak.men@pse.ngo>
2024-07-17 14:50:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [themeColor, setThemeColor] = useState("dark");
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "#333333";
-    document.body.style.color = "#ffffff";
-  }, []);
-
-  const changeAppThemeColor = (value) => {
-    if (value === "light") {
-      setThemeColor("ligth");
-      document.body.style.backgroundColor = "#ffffff";
-      document.body.style.color = "#333333";
-    } else {
-      setThemeColor("dark");
-      document.body.style.backgroundColor = "#333333";
-      document.body.style.color = "#ffffff";
-    }
-  };
-
   return (
     <div className="container">
       <h1>React</h1>
-      <div>Theme Color: {themeColor}</div>
-      <div>
-        <button onClick={() => changeAppThemeColor("dark")}>Dark</button>
-        <button onClick={() => changeAppThemeColor("light")}>Light</button>
-      </div>
     </div>
   );
 }
